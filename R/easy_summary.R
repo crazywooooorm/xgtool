fd68dc59ce9ff8a5c00449f11719a596983178bb
Xinghao Gu <xg2217@columbia.edu>
2018-04-20 10:26:10 -0400

    add confidence interval and changes in missing wt

--- R/easy_summary.R
+++ R/easy_summary.R
@@ -1,27 +1,29 @@
 #-- target is numerical, can it be categorical?
 easy_summary <- function(data, target, wt, ...) {
   if(missing(wt)) {
-    data <- data %>%
-      mutate(.wt = 1)
+    target <- dplyr::enquo(target)
 
-    target <- enquo(target)
-
-    data %>%
-      summarise(mean = weighted.mean(!!target, w = .wt, na.rm = T),
-                n = sum(!is.na(!!target)),
-                sum_wts = sum(.wt, na.rm = T),
-                sd = sqrt(sum(.wt * (!!target - mean)^2, na.rm = T) / (n - 1)),
-                se = sd/sqrt(n))
+    summary_table <- data %>%
+      dplyr::summarise(mean = mean(!!target, na.rm = T),
+                       n = sum(!is.na(!!target)),
+                       sum_wts = n,
+                       sd = sqrt(sum((!!target - mean)^2, na.rm = T)/(n - 1)),
+                       se = sd/sqrt(n))
   } else {
-    target <- enquo(target)
-    wt <- enquo(wt)
+    target <- dplyr::enquo(target)
+    wt <- dplyr::enquo(wt)
 
-    data %>%
-      summarise(mean = weighted.mean(!!target, w = !!wt, na.rm = T),
-                n = sum(!is.na(!!target)),
-                sum_wts = sum(!!wt, na.rm = T),
-                sd = sqrt(sum(wt * (!!target - mean)^2, na.rm = T) / (n - 1)),
-                se = sd/sqrt(n))
+    summary_table <- data %>%
+      dplyr::summarise(mean = weighted.mean(!!target, w = !!wt, na.rm = T),
+                       n = sum(!is.na(!!target)),
+                       sum_wts = sum(!!wt, na.rm = T),
+                       sd = sqrt(sum(wt * (!!target - mean)^2, na.rm = T)/(n - 1)),
+                       se = sd/sqrt(n))
   }
+
+  summary_table %>%
+    dplyr::mutate(ci_l = mean - stats::qnorm(0.975) * se,
+                  ci_u = mean + stats::qnorm(0.975) * se) %>%
+    dplyr::select(mean, se, n, sum_wts, ci_l, ci_u)
 }
 
